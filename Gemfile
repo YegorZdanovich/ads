2c6f480aa320ac40c18d60c6a4fd7e027ef6d6ac
YegorZdanovich <yegor.zdanovich@gmail.com>
2014-04-08 17:40:21 +0300

    install bootstrap and home page

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+
+gem "twitter-bootstrap-rails"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.4'
 
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Ads
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.test false
+      g.helper false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
