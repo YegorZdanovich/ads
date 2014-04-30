16d831cba5dee45e5a015cfcba14a139b560c335
YegorZdanovich <yegor.zdanovich@gmail.com>
2014-04-30 16:42:15 +0300

    fixes #11 and fixes #12

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,9 +1,10 @@
 class AdsController < ApplicationController
-  
+
   def index
 
     @search = Category.search(params[:q])
-     
+    @category = Category.includes(:advertisements)
+
     if params[:q].present?
       params[:facet] = nil
       @adss = Advertisement.order(params[:q][:s])
@@ -20,7 +21,7 @@ class AdsController < ApplicationController
       @adss = Category.find_by(value: params[:facet]).advertisements
     end
 
-    @adss = @adss.published.time_post_order.page(params[:page]).per_page(10)
+    @adss = @adss.includes(:category).published.time_post_order.page(params[:page]).per_page(10)
   end
 
 end
